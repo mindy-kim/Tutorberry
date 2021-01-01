b73f2759fcefe8daa4b29354ea98637f4a96e257
mindy-kim <39883887+mindy-kim@users.noreply.github.com>
2020-12-31 22:05:50 -0800

    Add files via upload

---
+++ Faq.js
@@ -0,0 +1,66 @@
+import React from "react";
+import styled from "styled-components";
+import { Container, Row, Col } from "react-bootstrap";
+
+import { Title, Section, Box, Text } from "../../components/Core";
+import data from "../../data.json";
+
+const SectionStyled = styled(Section)``;
+
+const FaqCard = styled(Box)`
+  padding-left: 55px;
+  position: relative;
+  &::before {
+    content: "\f061";
+    font-family: "Font Awesome 5 Free";
+    font-weight: 900;
+    min-width: 30px;
+    min-height: 30px;
+    max-height: 30px;
+    font-size: 15px;
+    background-color: ${({ theme }) => theme.colors.secondary};
+    color: ${({ theme }) => theme.colors.light};
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 500px;
+    position: absolute;
+    left: 0;
+  }
+`;
+
+const FaqTitle = styled(Title)`
+  color: ${({ theme }) => theme.colors.light};
+  font-size: 24px;
+  font-weight: 900;
+  letter-spacing: -0.75px;
+  margin-bottom: 24px;
+  line-height: 2rem;
+`;
+const { home: { faq: {questions}}} = data;
+
+const Faq = () => (
+  <>
+    {/* <!-- Faq section --> */}
+    <SectionStyled bg="ash">
+      <Container>
+        <Row>
+          {
+            questions.map(question => <Col lg="6" className="mb-5">
+              <FaqCard>
+                <div>
+                  <FaqTitle>{question.title}</FaqTitle>
+                  <Text variant="small" color="light" opacity={0.7}>
+                    {question.answer}
+                  </Text>
+                </div>
+              </FaqCard>
+            </Col>)
+          }
+        </Row>
+      </Container>
+    </SectionStyled>
+  </>
+);
+
+export default Faq;
---
+++ InfoSections.js
@@ -0,0 +1,59 @@
+import React from "react";
+import styled from "styled-components";
+import { Container, Row, Col } from "react-bootstrap";
+
+import { Title, Box, Text } from "../../components/Core";
+
+import studentRun from "../../assets/image/svg/student-run.svg";
+import fullyOnline from "../../assets/image/svg/fully-online.svg";
+import subjects from "../../assets/image/svg/subjects.svg";
+import data from '../../data.json';
+
+const BoxStyled = styled(Box)`
+  border-bottom: 1px solid #eae9f2;
+`;
+
+const IconImage = styled.img`
+  display: block;
+  max-height: 100px;
+`
+
+const { home: { info_section}} = data;
+
+
+const FeatureCard = ({ iconImage, title, children, ...rest }) => (
+  <Box {...rest} className="d-flex flex-column align-items-center">
+    <Box mb={[3, 3, 3, 4]} pb={2}>
+      <IconImage src={iconImage} alt="" />
+    </Box>
+    <Box className="text-center">
+      <Title variant="card" fontSize="24px" mb={3}>
+        {title}
+      </Title>
+      <Text variant="small">{children}</Text>
+    </Box>
+  </Box>
+);
+
+const images = [studentRun, fullyOnline, subjects]
+
+const InfoSections = () => (
+  <>
+    <BoxStyled bg="#f6f6f8" pb={["50px!important"]}>
+      <Container>
+        <Row className="justify-content-center pt-5">
+          {
+            info_section.features.map((feature, i) => <Col lg="4" className="mb-5 mb-lg-4">
+
+              <FeatureCard title={feature.title} iconImage={images[i]}>
+                {feature.description}
+              </FeatureCard>
+            </Col>)
+          }
+        </Row>
+      </Container>
+    </BoxStyled>
+  </>
+);
+
+export default InfoSections;
