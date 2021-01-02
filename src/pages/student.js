686d31884f02af0b62ffa5b3937ff0d1e216c266
mindy-kim <39883887+mindy-kim@users.noreply.github.com>
2021-01-02 14:39:46 -0800

    Update student.js

--- src/pages/student.js
+++ src/pages/student.js
@@ -3,7 +3,6 @@ import HowItWorks from "../sections/student/HowItWorks";
 import InfoSections from "../sections/student/InfoSections";
 import AvailableSubjects from "../sections/student/AvailableSubjects";
 import Reviews from "../sections/student/Reviews";
-import Author from "../sections/student/Author";
 import Offer from "../sections/student/Offer";
 import Faq from "../sections/student/Faq";
 import CTA from "../sections/student/CTA";
