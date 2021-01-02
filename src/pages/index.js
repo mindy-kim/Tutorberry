7a35fc7d0c1c766ce5ab640dfd7b78226b954066
mindy-kim <39883887+mindy-kim@users.noreply.github.com>
2021-01-02 13:14:09 -0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,5 @@
 import React from "react";
 import Banner from "../sections/home/Banner";
-import HowItWorks from "../sections/home/HowItWorks";
-import InfoSections from "../sections/home/InfoSections";
-import AvailableSubjects from "../sections/home/AvailableSubjects";
-import Reviews from "../sections/home/Reviews";
-import Author from "../sections/home/Author";
-import Offer from "../sections/home/Offer";
-import Faq from "../sections/home/Faq";
-import CTA from "../sections/home/CTA";
 import PageWrapper from "../components/PageWrapper";
 
 const LandingPage = () => {
