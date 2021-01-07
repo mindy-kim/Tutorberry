8ee7b2b5cb6738d30ea58f424f4ab580b4a2a20c
mindy-kim <39883887+mindy-kim@users.noreply.github.com>
2021-01-06 20:02:09 -0800

    Update contact.js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -7,8 +7,10 @@ import { Title, Button, Section, Box, Text, Input } from "../components/Core";
 import PageWrapper from "../components/PageWrapper";
 
 import Hero from "../sections/common/Hero";
+import data from "../data.json";
 
 const FormStyled = styled.form``;
+const { contact: {phone, email}} = data;
 
 const Contact = () => {
   return (
@@ -19,48 +21,15 @@ const Contact = () => {
         </Hero>
         <Section>
           <Container>
-            <Row className="align-items-center">
-              <Col lg="6">
-                <FormStyled>
-                  <Box mb={5}>
-                    <Title>We are always open 24/7 for you.</Title>
-                  </Box>
-                  <Box mb={3}>
-                    <Input type="text" placeholder="Your name" />
-                  </Box>
-                  <Box mb={3}>
-                    <Input type="email" placeholder="Email address" />
-                  </Box>
-
-                  <Box mb={3}>
-                    <Input type="text" placeholder="Subject" />
-                  </Box>
-
-                  <Box mb={3}>
-                    <Input
-                      type="text"
-                      as="textarea"
-                      placeholder="Write your message"
-                      rows={4}
-                    />
-                  </Box>
-
-                  <Button width="100%" type="submit" borderRadius={10}>
-                    Send Message
-                  </Button>
-                </FormStyled>
-              </Col>
-              <Col
-                lg={5}
-                className="offset-lg-1 order-lg-2 mt-5 mt-lg-0 pt-lg-5">
-                <Box className="mb-5">
-                  <Title variant="card" fontSize="24px">
-                    Email us
-                  </Title>
-                  <Text>tutorberryvolunteer@gmail.com</Text>
-                </Box>
-              </Col>
-            </Row>
+            <Box mb={5}>
+              <Title>We are always open 24/7 for you.</Title>
+            </Box>
+            <Box className="mb-5">
+              <Title variant="card" fontSize="24px">
+                Email us
+              </Title>
+              <Text>{email}</Text>
+            </Box>
           </Container>
         </Section>
       </PageWrapper>
