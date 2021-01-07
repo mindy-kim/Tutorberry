514555380f89e2cd8ac38d0cf944b980eb235365
mindy-kim <39883887+mindy-kim@users.noreply.github.com>
2021-01-06 20:29:47 -0800

    Update InfoSections.js

--- src/sections/student/InfoSections.js
+++ src/sections/student/InfoSections.js
@@ -7,6 +7,7 @@ import { Title, Box, Text } from "../../components/Core";
 import studentRun from "../../assets/image/svg/student-run.svg";
 import fullyOnline from "../../assets/image/svg/fully-online.svg";
 import subjects from "../../assets/image/svg/subjects.svg";
+import data from '../../data.json';
 
 const BoxStyled = styled(Box)`
   border-bottom: 1px solid #eae9f2;
@@ -16,6 +17,9 @@ const IconImage = styled.img`
   display: block;
   max-height: 100px;
 `
+
+const { home: { info_section}} = data;
+
 
 const FeatureCard = ({ iconImage, title, children, ...rest }) => (
   <Box {...rest} className="d-flex flex-column align-items-center">
@@ -31,29 +35,21 @@ const FeatureCard = ({ iconImage, title, children, ...rest }) => (
   </Box>
 );
 
+const images = [studentRun, fullyOnline, subjects]
+
 const InfoSections = () => (
   <>
     <BoxStyled bg="#f6f6f8" pb={["50px!important"]}>
       <Container>
         <Row className="justify-content-center pt-5">
-          <Col lg="4" className="mb-5 mb-lg-4">
-            <FeatureCard title="Fully Student Run" iconImage={studentRun}>
-              All tutors are experienced highschool students with backgrounds in a variety of subjects.
-              Every tutor has the experience and education necessary to guide students through difficult subjects.
-            </FeatureCard>
-          </Col>
-          <Col lg="4" className="mb-5 mb-lg-4">
-            <FeatureCard title="Virtual Sessions" iconImage={fullyOnline}>
-              Students meet with tutors one-on-one through video calls with the Zoom platform.
-              Communication is crucial, and Tutorberry will maintain close contact and always be open for questions or comments.
-            </FeatureCard>
-          </Col>
-          <Col lg="4" className="mb-5 mb-lg-4">
-            <FeatureCard title="Wide Variety of Subjects" iconImage={subjects}>
-              Our tutors are experienced in and prepared to teach a variety of subjects, such as Math, Language Arts, Foreign Studies, and Social Studies.
-              Each tutor will work closely with the student.
-            </FeatureCard>
-          </Col>
+          {
+            info_section.features.map((feature, i) => <Col lg="4" className="mb-5 mb-lg-4">
+
+              <FeatureCard title={feature.title} iconImage={images[i]}>
+                {feature.description}
+              </FeatureCard>
+            </Col>)
+          }
         </Row>
       </Container>
     </BoxStyled>
