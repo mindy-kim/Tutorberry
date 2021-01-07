bcc2e805b81cb0ef65567a5403ba3eaa86a705b3
mindy-kim <39883887+mindy-kim@users.noreply.github.com>
2021-01-07 01:41:30 -0800

    Update volunteer.js

--- src/pages/volunteer.js
+++ src/pages/volunteer.js
@@ -2,7 +2,6 @@ import React from "react";
 import HowItWorks from "../sections/volunteer/HowItWorks";
 import AvailableSubjects from "../sections/volunteer/AvailableSubjects";
 import Reviews from "../sections/volunteer/Reviews";
-import Offer from "../sections/volunteer/Offer";
 import Faq from "../sections/volunteer/Faq";
 import CTA from "../sections/volunteer/CTA";
 import PageWrapper from "../components/PageWrapper";
