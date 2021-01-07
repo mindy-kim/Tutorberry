c9345e47e6363ebca0d11fa650c4559e56414154
txdk1m <74169727+txdk1m@users.noreply.github.com>
2021-01-06 19:56:28 -0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Banner from "../sections/home/Banner";
 import PageWrapper from "../components/PageWrapper";
+import InfoSections from "../sections/student/InfoSections";
 
 const LandingPage = () => {
   return (
     <>
       <PageWrapper>
         <Banner />
+        <InfoSections />
       </PageWrapper>
     </>
   );
